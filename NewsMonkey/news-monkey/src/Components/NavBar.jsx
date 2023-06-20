737daa0d5b0ec1d33b4da14252db8b9c22a302bc
Abhijit <abhijitmandal.btech21@sushantuniversity.edu.in>
2023-06-20 12:35:25 +0530

    Added Badges

--- NewsMonkey/news-monkey/src/Components/NavBar.jsx
+++ NewsMonkey/news-monkey/src/Components/NavBar.jsx
@@ -15,7 +15,7 @@ export class NavBar extends Component {
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
+                        <li className="nav-item">
                             <Link className="nav-link" to="/">Trending </Link>
                         </li>
                         <li className="nav-item">
--- NewsMonkey/news-monkey/src/Components/NewsItem.jsx
+++ NewsMonkey/news-monkey/src/Components/NewsItem.jsx
@@ -3,15 +3,19 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
     render() {
-        let { title, description, imageUrl, newsUrl} = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className="my-3">
                 <div className="card-box card">
-                    <img className="card-img-top" src={!imageUrl ?"https://www.mca.org.uk/wp-content/themes/consultix/images/no-image-found-360x260.png":imageUrl} alt='' />
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-dark">
+                        <span className="visually-hidden">{source}</span>
+                    </span>
+                    <img className="card-img-top" src={!imageUrl ? "https://www.mca.org.uk/wp-content/themes/consultix/images/no-image-found-360x260.png" : imageUrl} alt='' />
                     <div className="card-body">
                         <h5 className="card-title"><strong>{title}</strong></h5>
                         <p className="card-text">{description}</p>
-                        <a rel="noreferrer" href={newsUrl} target="_blank" className="read-btn btn btn-dark">Read More &rarr;</a>
+                        <p className="card-text"><small className="text-body-secondary">By <strong>{author ? author : "Unknown"}</strong> on <strong>{new Date(date).toGMTString()}</strong></small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="read-btn btn btn-dark">Read News &rarr;</a>
                     </div>
                 </div>
             </div>
@@ -20,7 +24,3 @@ export class NewsItem extends Component {
 }
 
 export default NewsItem
-
-
-// image not found error.
-// https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg
